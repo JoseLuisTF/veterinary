7220374ecc912a21529c1f5f7fa85b6bf2e2618a
Jose Tarazona <joseluistf@ufps.edu.co>
2019-10-21 14:49:02 -0500

    local

---
+++ apps/mascota/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from apps.mascota.models import Raza, Especie, Mascota
+# Register your models here.
+admin.site.register(Raza)
+admin.site.register(Especie)
+admin.site.register(Mascota)
---
+++ apps/mascota/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Especie(models.Model):
+    id_especie = models.AutoField(primary_key=True)
+    nom_especie = models.CharField(max_length=15)
+
+    def __str__(self):
+        return '{}'.format(self.nom_especie)
+
+
+class Raza(models.Model):
+    id_raza = models.AutoField(primary_key=True)
+    nom_raza = models.CharField(max_length=15)
+    especie = models.ForeignKey(Especie, null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}'.format(self.nom_raza)
+
+
+class Mascota(models.Model):
+    id_raza = models.AutoField(primary_key=True)
+    alias = models.CharField(max_length=15)
+    especie = models.ForeignKey(Especie, null=True, blank=True, on_delete=models.CASCADE)
+    raza = models.ForeignKey(Raza, null=True, blank=True, on_delete=models.CASCADE)
+    sexo = models.CharField(max_length=15)
+    edad = models.IntegerField()
+
+    def __str__(self):
+        return '{}'.format(self.alias)
+
+
+
