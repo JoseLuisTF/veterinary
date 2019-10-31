66feef19ee094526e10b3ec4f89b49904972b534
Jose Tarazona <joseluistf@ufps.edu.co>
2019-10-30 23:28:33 -0500

    correccion host

--- Veterinary/settings/server.py
+++ Veterinary/settings/server.py
@@ -4,7 +4,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['veterinaryjoseluistf.herokuapp.com']
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
